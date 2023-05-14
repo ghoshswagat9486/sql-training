78703e183ce817917a74789523746be82d70ddb4
ghoshswagat9486 <51303128+ghoshswagat9486@users.noreply.github.com>
2023-05-14 02:59:29 -0400

    Create Exercise1.sql

---
+++ Week1/Exercise1.sql
@@ -0,0 +1,3 @@
+Fetch results from the agents table for agents who live in Bangalore.
+Fetch customers who have a grade of 2 and do not live in London.
+Fetch all orders which have an amount greater than 3000.
