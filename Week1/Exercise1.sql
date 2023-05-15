d9fac14fb3f77d81e811783052d5b5ce8ea87da7
ghoshswagat9486 <51303128+ghoshswagat9486@users.noreply.github.com>
2023-05-15 12:08:05 -0400

    Update Exercise1.sql

--- Week1/Exercise1.sql
+++ Week1/Exercise1.sql
@@ -1,3 +1,5 @@
 Fetch results from the agents table for agents who live in Bangalore.
 Fetch customers who have a grade of 2 and do not live in London.
 Fetch all orders which have an amount greater than 3000.
+Fetch order number and amount for all orders from the daysorder table that have an order date greater than 2008-07-15 and have an order amount 
+greater than 1200.
