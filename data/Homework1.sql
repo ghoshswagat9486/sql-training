d7049b8ad01342584b4e5400de3d4878c449d454
ghoshswagat9486 <51303128+ghoshswagat9486@users.noreply.github.com>
2023-05-19 13:09:14 -0400

    Create Homework1.sql

---
+++ data/Homework1.sql
@@ -0,0 +1,3 @@
+1) print all records from daysorder where ord_amount is greater or equal to thrice of advance_amount 
+2) print all records from daysorder where ord_amount is greater or equal to thrice of 37.25 
+3) print the title of students from the student table  who are in the 5th grade
